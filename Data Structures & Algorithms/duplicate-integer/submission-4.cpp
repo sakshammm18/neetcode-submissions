d24b9f2e32e0f880b826d41e26445b3b8438b305
sakshammm18 <hkrsaksham@gmail.com>
2026-07-09 15:23:03 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> mp;
+        for(auto &it: nums){
+            if(mp.find(it)!=mp.end()){
+                return true;
+            }
+            else 
+            mp.insert(it);
+        }
+        return false;
+    }
+};
