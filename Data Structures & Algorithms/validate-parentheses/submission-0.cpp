e28ebf1e515e62c83997b322ff8ee755322c7225
sakshammm18 <hkrsaksham@gmail.com>
2026-07-11 12:11:38 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+         stack<int>st;
+         for(int i =0;i<s.size();i++){
+            char ch=s[i];
+         if(ch=='('||ch=='{'||ch=='[')
+           st.push(ch);
+         
+         else{
+            if(!st.empty()){
+                char topCh=st.top();
+                if(ch==')' && topCh=='(')
+                st.pop();  
+                else if(ch=='}' && topCh=='{')
+                st.pop();
+                else if(ch==']' && topCh=='[')
+                st.pop();   
+                else{
+                    return false;
+                }    
+            }
+            else{
+                return false;
+            }
+         }
+         }
+         if(st.empty()){
+          return true;
+         }
+         else {
+            return false;
+         }
+    }
+};
